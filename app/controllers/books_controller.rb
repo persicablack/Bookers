4a51060c412972c9ec1b9ebde6cd1cf3da93d4be
persica <129649042+persicablack@users.noreply.github.com>
2023-04-18 11:15:02 +0000

    [Finish] レイアウト整理

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,8 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(@book.id)
+       flash[:notice] = "Book was successfully created."
+       redirect_to book_path(@book.id)
     else
       @books = Book.all
       render :index
@@ -25,7 +26,8 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book.id)
+       flash[:notice] = "Book was successfully updated."
+       redirect_to book_path(@book.id)
     else
       @books = Book.all
       render :edit
@@ -35,6 +37,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
   
